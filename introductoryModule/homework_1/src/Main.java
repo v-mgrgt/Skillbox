4a53846905b017c14dbfdd3c6d61186ec324412c
Vyacheslav <informationsss@ya.ru>
2022-05-28 14:52:57 +0300

    Инициализация проекта

---
+++ introductoryModule/homework_1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("СВЕДЕНИЯ О СИСТЕМЕ");
+        System.out.print("Операционная система: ");
+        System.out.println(System.getProperty("os.name") + " (версия: " + System.getProperty("os.version") + ")");
+        System.out.print("Количество процессоров, доступных JVM: ");
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        System.out.print("Объем памяти, доступной JVM: ");
+        System.out.println(Runtime.getRuntime().maxMemory() / 1024 / 1024 + " Мб");
+    }
+}
