c9a7f220eb3b75d22bd90a845427c3bcf285874e
Vyacheslav <informationsss@ya.ru>
2022-09-29 16:13:09 +0300

    Выполнение задачи № 14

---
+++ objects-classes-part2/README.md
@@ -0,0 +1,4 @@
+# 5. Объекты и классы. Часть 2. Инкапсуляция
+
+* [Задание 1: Elevator](https://github.com/v-mgrgt/Skillbox/tree/main/objects-classes-part2/homework_14)
+* [Задание 2: Cargo](https://github.com/v-mgrgt/Skillbox/tree/main/objects-classes-part2/homework_15)
---
+++ objects-classes-part2/homework_14/src/Elevator.java
@@ -0,0 +1,40 @@
+public class Elevator {
+    private final int minFloor;
+    private final int maxFloor;
+    private int currentFloor = 1;
+
+    public Elevator(int minFloor, int maxFloor) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+    }
+
+    public void moveUp() {
+        currentFloor += 1;
+    }
+
+    public void moveDown() {
+        currentFloor -= 1;
+    }
+
+    public void move(int floor) {
+        if (floor >= minFloor && floor <= maxFloor && floor != currentFloor) {
+            if (floor > currentFloor) {
+                while (floor != currentFloor) {
+                    moveUp();
+                    System.out.println("Текущий этаж: " + currentFloor);
+                }
+            } else {
+                while (floor != currentFloor) {
+                    moveDown();
+                    System.out.println("Текущий этаж: " + currentFloor);
+                }
+            }
+        } else {
+            System.out.println("Ошибка ввода номера этажа");
+        }
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+}
